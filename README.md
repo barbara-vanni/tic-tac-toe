efaad51a26e101452f3bfc0cdc08d15be253cfe9
Dhjali <barbara.vanni@laplateforme.io>
2023-12-05 15:57:01 +0100

    adding img

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # tic-tac-toe
+
+
+PB à régler : 
+
+- Je ne peux faire qu'une seule new-game 
+- marge blanche en haut de la fenetre 
+- pas de compteur pour les points 
--- main.py
+++ main.py
@@ -2,62 +2,107 @@
 
 import tkinter as tk
 from tkinter import *
-
+from PIL import Image, ImageTk
+from numpy import imag
 
 # Identifier les 9 cases de mon jeu
-# grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 grid = [["", "", ""], ["", "", ""], ["", "", ""]]
 # Identifier les joueurs
 player = 1
+win_X=0
+win_Y=0
+tie = 0
 
 # Création et ouverture fenêtre tkinter
 window = tk.Tk()
 window.title("TIC.TAC.TOE")
-window.geometry("800x800")
+window.geometry("1000x800")
 
-center_frame = Frame(window)
+
+center_frame = tk.Frame(window)
 center_frame.grid(row=0, column=0, padx=35, pady=50)
 
+message_frame = tk.Frame(window)  
+message_frame.grid(row=0, column=0, sticky="ew") 
 
 # Variable pour compter le nombre de coups
 count = 0
+
+# images à la place du x et du o
+image_x = ImageTk.PhotoImage(Image.open("./assets/X_flamme.png"))
+image_o = ImageTk.PhotoImage(Image.open("./assets/O_flamme.png"))
+
 
 # Fonction appelée lorsqu'un bouton est cliqué
 def on_button_click(row, col):
     global player, count
     if grid[row][col] == "" and count < 9:
         if player == 1:
-            text = "X"
+            image = image_o
             player = 2
         else:
-            text = "O"
+            image = image_x
             player = 1
-        btn = tk.Button(center_frame, text=text, borderwidth=2, relief=tk.GROOVE, font=("Arial", 50), height=2, width=6, bg="white", fg="black", cursor="heart")
+
+        btn = tk.Button(center_frame, image=image, borderwidth=0, relief=tk.GROOVE, height=200, width=200,bg="white", cursor="heart")
         btn.grid(row=row, column=col)
-        grid[row][col] = text
+        grid[row][col] = player  
         count += 1
         check_winner()
 
-
 # combinaison gagnante 
 def check_winner():
-    win= []
     for x in range(3):
         # check pour les lignes et les colonnes 
         if grid[x][0] == grid[x][1] == grid[x][2] != "":
-            win += grid[x][0]
+            win(grid[x][0])
             return
         if grid[0][x] == grid[1][x] == grid[2][x] != "":
-            win += grid[0][x]
+            win(grid[0][x])
             return
     # check pour les deux diagonales
     if grid[0][0] == grid[1][1] == grid[2][2] != "":
-        win += grid[0][0]
+        win(grid[0][0])
         return
     if grid[0][2] == grid[1][1] == grid[2][0] != "":
-        win += grid[0][2]
+        win(grid[0][2])
         return
+
+
+# Fonction pour vérifier s'il y a match nul
+def check_tie_game():
+    global tie
+    if count == 9 and not check_winner():
+        msg = tk.Message(message_frame, text = "Match nul !")
+        msg.config(bg='white', font=('times', 40), borderwidth=0)
+        msg.grid()
+        tie += 1
+        reset_game()
+        return True
+    return False
+
+
+# Fonction pour réinitialiser le jeu
+def reset_game():
+    global grid, player, count
+    grid = [["", "", ""], ["", "", ""], ["", "", ""]]
+    player = 1
+    count = 0
+    create_board()  
     
+# message gagnant
+def win(winner):
+    global player, win_X, win_Y
+    if player == 1 :
+        win_X += 1
+    else :
+        win_Y += 1
+        
+    msg = tk.Message(message_frame, text = f"Victoire !Le joueur {winner} a gagné!")
+    msg.config(bg='white', font=('times', 40),  borderwidth=0)
+    msg.grid()
+    update_scores()
+    reset_game()
 
 # Création de la grille de jeu
 def create_board():
@@ -66,16 +111,75 @@ def create_board():
             btn = tk.Button(center_frame, text="", borderwidth=2, relief=tk.GROOVE, font=("Arial", 50), height=2, width=6, bg="white", fg="black", cursor="heart", command=lambda i=i, j=j: on_button_click(i, j))
             btn.grid(row=i, column=j)
 
-create_board()
+def show_game():
+    # Fonction pour afficher la grille de jeu
+    create_board()
+    quit_button.grid(row=4, column=0, columnspan=1, pady=10)
+    new_game_button.grid(row=4, column=0, columnspan=10, pady=0)
 
-# Ajouter un bouton pour quitter
+def start_game():
+    # Fonction pour démarrer le jeu depuis la page d'accueil
+    welcome_label.grid_forget()
+    start_button.grid_forget()
+    canvas.destroy()
+    show_game()
+
+def new_game():
+    if message_frame.winfo_exists():
+        message_frame.destroy()
+
+    reset_game()
+    show_game()
+
+# Ajouter deux étiquettes pour afficher les scores
+label_X = tk.Label(window, text="Joueur X: 0", font=('times', 20), bg='blue')
+label_Y = tk.Label(window, text="Joueur O: 0", font=('times', 20), bg='blue')
+label_X.grid(row=0, column=1,sticky="E")
+label_Y.grid(row=1, column=1,sticky="E")
+
+
+# Fonction pour réinitialiser les scores
+def reset_scores():
+    global win_X, win_Y
+    win_X = 0
+    win_Y = 0
+    update_scores()
+
+# Fonction pour mettre à jour les étiquettes des scores
+def update_scores():
+    label_X.config(text=f"Joueur X: {win_X}")
+    label_Y.config(text=f"Joueur O: {win_Y}")
+
+def game_loop():
+
+    check_tie_game()
+    check_winner()
+    window.after(100, game_loop)# Appel récursif pour continuer la boucle
+
+# Page d'accueil
+bg_image = ImageTk.PhotoImage(Image.open("./assets/tictac.png"))
+welcome_label = tk.Label(window, image=bg_image)
+welcome_label.grid()
+
+
+# Création du canevas pour afficher l'image de fond
+canvas = tk.Canvas(window, width=bg_image.width(), height=bg_image.height())
+canvas.grid()
+
+# Affichage de l'image de fond sur le canevas
+canvas.create_image(0, 0, image=bg_image, anchor=tk.NW)
+
+
+photo = PhotoImage(file='./assets/start_flamme.png')
+start_button = tk.Button(window,image=photo, command=start_game)
+start_button.grid(row=1, column=0)
+
+# Bouton pour quitter
 quit_button = tk.Button(center_frame, text="Quit", command=window.destroy)
-quit_button.grid(row=4, column=0, columnspan=1, pady=10)
-# ajouter bouton nouvelle partie
-new_game_button = tk.Button(center_frame, text="New Game")
-new_game_button.grid(row=4, column=0, columnspan=10, pady=0)
 
+# Bouton nouvelle partie
+new_game_button = tk.Button(center_frame, text="New Game", command=new_game)
+
+# Lancement de la boucle principale
+window.after(100, game_loop)
 window.mainloop()
-
-
- 
